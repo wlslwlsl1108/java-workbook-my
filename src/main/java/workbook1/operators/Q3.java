3423f13d6ac4488357dcdcada3faa5311c27104e
jinhee kim <lsa68803@naver.com>
2025-08-06 02:12:45 +0900

    feat: [operators] Q3 구현

--- src/main/java/workbook1/operators/Q3.java
+++ src/main/java/workbook1/operators/Q3.java
@@ -8,5 +8,10 @@ public class Q3 {
         double x = sc.nextDouble();
         double y = sc.nextDouble();
         // TODO: x &gt; y 결과와 !(x &gt; y) 결과 출력
+
+        boolean isGreater = (x > y); // 첫번째 값이 두번째 값보다 큰지 비교 (비교 연산자 : >)
+        boolean isNotGreater = !(x > y); // 논리 NOT 연산 적용 (논리 연산자 : !)
+
+        System.out.println(isGreater + " " + isNotGreater);
     }
 }
