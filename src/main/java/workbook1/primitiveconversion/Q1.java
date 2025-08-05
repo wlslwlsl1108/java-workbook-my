312efc9cb8acee7ed8e03f6cc9aacbc78550b26a
jinhee kim <lsa68803@naver.com>
2025-08-06 00:04:40 +0900

    feat: [primitiveconversion] Q1 구현

--- src/main/java/workbook1/primitiveconversion/Q1.java
+++ src/main/java/workbook1/primitiveconversion/Q1.java
@@ -8,5 +8,12 @@ public class Q1 {
         int a = sc.nextInt();
         int b = sc.nextInt();
         // TODO: a와 b를 double로 자동 형변환하여 더한 뒤 int로 강제 변환하여 출력
+        // double dA = a; -> a 자동 형변환
+        // double dB = b; -> b 자동 형변환
+
+        double sum = a + b; // a, b 연산 중 자동 형 변환
+        int iSum = (int)sum; // double -> int 강제 형 변환
+
+        System.out.println(iSum); // 출력
     }
 }
