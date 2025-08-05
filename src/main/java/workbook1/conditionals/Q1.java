75229d3f5df59bc06444844365b436d4eab16bc3
jinhee kim <lsa68803@naver.com>
2025-08-06 03:08:54 +0900

    feat: [conditionals] Q1 구현

--- src/main/java/workbook1/conditionals/Q1.java
+++ src/main/java/workbook1/conditionals/Q1.java
@@ -7,5 +7,14 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         // TODO: num의 부호를 판단하여 출력
+        if (num < 0){
+            System.out.println("음수"); // num < 0 이면 "음수" 출력
+
+        } else if (num == 0) {
+            System.out.println("0"); // num == 0 이면 "0" 출력
+
+        } else {
+            System.out.println("양수"); // num > 0 이면 "양수" 출력
+        }
     }
 }
