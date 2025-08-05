b4eb9b935bbe628abe76e4a949bb9276e5051d3b
jinhee kim <lsa68803@naver.com>
2025-08-06 00:28:11 +0900

    feat: [primitiveconversion] Q2 구현

--- src/main/java/workbook1/primitiveconversion/Q2.java
+++ src/main/java/workbook1/primitiveconversion/Q2.java
@@ -7,5 +7,10 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         double d = sc.nextDouble();
         // TODO: d를 int로 강제 형변환하여 차이를 계산 후 출력
+        int iD = (int) d; // int로 강제 형변환
+        double sub = d - iD; // double d 와 int iD 차이 계산
+
+        // System.out.println(sub) 출력 시 출력 예시처럼 출력안됨
+        System.out.printf("%.2f", sub); // 출력(소숫점 둘째자리까지)
     }
 }
