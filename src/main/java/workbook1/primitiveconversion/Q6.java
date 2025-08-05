f3c329743e21a5ae37ebf16ea9c163e15b194eae
jinhee kim <lsa68803@naver.com>
2025-08-06 01:38:42 +0900

    feat: [primitiveconversion] Q6 구현

--- src/main/java/workbook1/primitiveconversion/Q6.java
+++ src/main/java/workbook1/primitiveconversion/Q6.java
@@ -8,5 +8,11 @@ public class Q6 {
         String s1 = sc.next();
         String s2 = sc.next();
         // TODO: s1, s2를 int로 변환 후 차이를 byte로 변환하여 출력
+        int Is1 = Integer.parseInt(s1); // s1 형변환 (String -> int)
+        int Is2 = Integer.parseInt(s2); // s2 형변환 (String -> int)
+
+        byte sub = (byte)(Is1 - Is2); // 두 수의 차 강제 형 변환 (int -> byte)
+
+        System.out.println(sub); // 두 수의 차(byte) 출력
     }
 }
