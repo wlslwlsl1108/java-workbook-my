94274a5f7cca799e780cae95ac84d782b74d83ec
jinhee kim <lsa68803@naver.com>
2025-08-06 01:58:22 +0900

    feat: [operators] Q2 구현

--- src/main/java/workbook1/operators/Q2.java
+++ src/main/java/workbook1/operators/Q2.java
@@ -7,5 +7,7 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         // TODO: 전위 증가와 후위 증가 결과 출력
+        System.out.print(++num + " "); // 전위 증가 출력
+        System.out.println(num++); // 후위 결과 출력 ( 후위 결과 num = 7 )
     }
 }
