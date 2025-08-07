f405450cd6f84253ae82a2e09f6797a74c1e6da4
jinhee kim <lsa68803@naver.com>
2025-08-07 22:46:34 +0900

    feat: [loops] Q5 구현

--- src/main/java/workbook1/loops/Q5.java
+++ src/main/java/workbook1/loops/Q5.java
@@ -7,5 +7,12 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         // TODO: 역삼각형 패턴 출력
+        // 중첩 'for' 문 사용
+        for (int i=h;i>=1;i--){        // 첫번째 for문 -> i : 행 번호 (h부터 1까지 감소)
+            for(int j=1;j<=i;j++){     // 두번째 for문 -> j : 열 번호 (1부터 i까지 반족)
+                System.out.print("*"); // 각 열마다 * 출력
+            }
+            System.out.println();      // 한 행 출력 후 줄 바꿈
+        }
     }
 }
