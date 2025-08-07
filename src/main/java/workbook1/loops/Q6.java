1a495bedb863135fe9af529c9c1c4d0381ef6cc8
jinhee kim <lsa68803@naver.com>
2025-08-07 23:01:02 +0900

    feat: [loops] Q6 구현

--- src/main/java/workbook1/loops/Q6.java
+++ src/main/java/workbook1/loops/Q6.java
@@ -1,7 +1,17 @@
 package main.java.workbook1.loops;
 
+import java.util.Scanner;
+
 public class Q6 {
     public static void main(String[] args) {
         // TODO: 구구단 전체를 출력
+
+        // j * i = (j*i) 한 행씩 출력
+        for(int i=1;i<=9;i++){                            // i : 1 ~ 9 까지 출력 (곱해지는 수)
+            for(int j=2;j<=9;j++){                        // j : 2 ~ 9 까지 출력 (단 -> 2단부터 출력)
+                System.out.print(j+"*"+i+"="+(j*i)+" ");  // j * i = 결과(j*i) + " " 공백까지 출력
+            }
+            System.out.println();                         // 한 행 출력 후 줄바꿈
+        }
     }
 }
