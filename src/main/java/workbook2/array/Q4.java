a6611b51d52a757aef00d6f7c35d9e3c7380432f
jinhee kim <lsa68803@naver.com>
2025-08-11 15:32:24 +0900

    feat: [array] Q4 구현

--- src/main/java/workbook2/array/Q4.java
+++ src/main/java/workbook2/array/Q4.java
@@ -9,5 +9,35 @@ public class Q4 {
         int[][] B = new int[3][3];
         int[][] C = new int[3][3];
         // TODO: A, B에 값 저장 후 C = A + B 계산, C 출력
+
+        // A에 값 저장
+        for (int i = 0; i < A.length; i++) {          // 행 개수만큼 저장
+            for (int j = 0; j < A[i].length; j++) {   // 해당 행의 열 개수만큼 저장
+                A[i][j] = sc.nextInt();               // 정수 입력받아 A에 저장
+            }
+        }
+
+        // B에 값 저장
+        for (int i = 0; i < B.length; i++) {          // 행 개수만큼 저장
+            for (int j = 0; j < B[i].length; j++) {   // 해당 행의 열 개수만큼 저장
+                B[i][j] = sc.nextInt();               // 정수 입력받아 B에 저장
+            }
+        }
+
+        // C 계산
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < C[i].length; j++) {
+                C[i][j] = A[i][j] + B[i][j];          // C = A + B 계산
+            }
+        }
+        // C 출력
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {   // C 요소 출력
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();                     // 한 행 종료시 개행
+        }
+        // 입력닫기
+        sc.close();
     }
 }
