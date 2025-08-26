302c4b3e4f2c0279b7ead55ecd3b7232f68e4250
jinhee kim <lsa68803@naver.com>
2025-08-26 15:56:07 +0900

    feat: [varinit] Q3 구현

--- src/main/java/workbook3/varinit/Q3.java
+++ src/main/java/workbook3/varinit/Q3.java
@@ -18,3 +18,32 @@ public class Q3 {
         System.out.println("instance variable");
     }
 }
+/*
+    [ 실행 순서 ]
+
+    1. 클래스 로딩 시점 -> static 변수/블록 실행 (1번만)
+        1) static 변수
+        2) static 변수  => "static block"
+
+    2. main 메서드 실행
+        - 첫줄 출력문 실행 => "static variable"
+
+    3. 객체 생성 (new Q3())
+        1) 인스턴스 변수/블록 실행 => "instance block"
+        2) 생성자 실행 => "constructor"
+
+    4. main 메서드 이어서 실행
+        - 다음 출력문 실행 => "instance variable"
+
+    ex. Q3 obj1 = new Q3();
+        System.out.println("instance variable"); 2번 하는 경우
+
+        => static block
+           static variable
+           instance block
+           constructor
+           instance variable
+           instance block
+           constructor
+           instance variable
+ */
