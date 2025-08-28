1cdd2f0d115294c7e5ed9c49521c138e70963c32
jinhee kim <lsa68803@naver.com>
2025-08-28 10:48:07 +0900

    feat: [inheritanceoverride] Q1 구현

--- src/main/java/workbook3/inheritanceoverride/Q1.java
+++ src/main/java/workbook3/inheritanceoverride/Q1.java
@@ -1,26 +1,26 @@
 package main.java.workbook3.inheritanceoverride;
 
-class Animal {
-    String name;
-    void eat() {
-        System.out.println("Eating...");
+class Animal {                                    // Animal 클래스 -> 부모 클래스
+    String name;                                  // 인스턴스 변수 name 선언
+    void eat() {                                  // eat() 메서드 생성
+        System.out.println("Eating...");          // Eating... 출력
     }
 }
 
-class Dog extends Animal {
-    public Dog() {
-        super.name = "Dog"; // super 키워드 맛보기
+class Dog extends Animal {                         // Dog 클래스 -> 자식 클래스 (Animal 상속)
+    public Dog() {                                 // Dog() 메서드 생성
+        super.name = "Dog"; // super 키워드 맛보기   // 부모 클래스의 name 필드를 Dog로 초기화
     }
-    void bark() {
-        System.out.println("Barking...");
+    void bark() {                                  // bark() 메서드 생성
+        System.out.println("Barking...");          // Barking 출력
     }
 }
 
 public class Q1 {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        System.out.println("dog.name = " + dog.name);
-        dog.eat();
-        dog.bark();
+        Dog dog = new Dog();                               // Dog() 객체 생성하여 dog가 참조
+        System.out.println("dog.name = " + dog.name);      // dog.name 출력
+        dog.eat();                                         // Dog가 Animal 상속했기 때문에 eat() 메서드 호출 가능
+        dog.bark();                                        // dog가 참조하는 bark() 메서드 호출
     }
 }
