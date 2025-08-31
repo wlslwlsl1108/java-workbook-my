7230b7434b7f850e37377a8628eeec9b5b4f4d4c
jinhee kim <lsa68803@naver.com>
2025-09-01 00:21:32 +0900

    fix: [thissuper] Q2 코드 및 주석 수정

--- src/main/java/workbook3/thissuper/Q2.java
+++ src/main/java/workbook3/thissuper/Q2.java
@@ -6,9 +6,7 @@ class Box {
     int width, height, depth;
     Box() {
         // TODO: 매개변수 생성자 호출하여 1,1,1로 초기화
-        this.width = 1;                    // width 생성자 호출하여 1로 초기화
-        this.height = 1;                   // height 생성자 호출하여 1로 초기화
-        this.depth = 1;                    // depth 생성자 호출하여 1로 초기화
+        this(1,1,1);              // 매개변수 생성자 호출하여 1,1,1로 초기화
     }
     Box(int w, int h, int d) {
         // TODO: 멤버 초기화
@@ -26,7 +24,7 @@ public class Q2 {
         int d = sc.nextInt();                                              // d 입력
         Box b1 = new Box();                                                // Box() 객체 생성하여 b1이 참조
         Box b2 = new Box(w, h, d);                                         // Box(w, h, d) 객체 생성하여 b2가 참조
-        System.out.println(b1.width + " " + b1.height + " " + b1.depth);   // b1가 참조하는 w / h / d 출력
-        System.out.println(b2.width + " " + b2.height + " " + b2.depth);   // b2가 참조하는 w / h / d 출력
+        System.out.println(b1.width + " " + b1.height + " " + b1.depth);   // b1 객체의 width / height / depth 출력
+        System.out.println(b2.width + " " + b2.height + " " + b2.depth);   // b2 객체의 width / height / depth 출력
     }
 }
