84c69dafa50e59e5ed819d4587362d4ed5fb39a1
jinhee kim <lsa68803@naver.com>
2025-09-01 03:30:17 +0900

    feat: [accesslevel] Q3 구현

--- src/main/java/workbook3/accesslevel/Q3.java
+++ src/main/java/workbook3/accesslevel/Q3.java
@@ -2,35 +2,36 @@ package main.java.workbook3.accesslevel;
 
 import java.util.Scanner;
 
-class Employee {
-    public String name;
-    private int salary;
+class Employee {                                // 클래스 정의
+    public String name;                         // 인스턴스 필드 선언 -> public (직접 접근 가능)
+    private int salary;                         // 인스턴스 필드 선언 -> private (직접 접근 불가)
 
-    Employee(String name, int salary) {
-        this.name = name;
+    Employee(String name, int salary) {         // 생성자 정의
+        this.name = name;                       // 전달받은 값으로 초기화
         this.salary = salary;
     }
 
-    public int getSalary() {
-        return salary;
+    public int getSalary() {                    // get 메서드 (getter)
+        return salary;                          // salary 반환
     }
 
-    public void setSalary(int salary) {
+    public void setSalary(int salary) {         // set 메서드 (setter)
         // TODO: salary 설정
+        this.salary = salary;                   // 전달받은 값으로 salary 값 설정
     }
 }
 
 public class Q3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n1 = sc.next();
-        int s1 = sc.nextInt();
-        String n2 = sc.next();
-        int s2 = sc.nextInt();
-        int raise = sc.nextInt();
-        Employee e1 = new Employee(n1, s1);
-        Employee e2 = new Employee(n2, s2);
-        e1.setSalary(e1.getSalary() + raise);
-        System.out.println(e1.getSalary() + " " + e2.getSalary());
+        Scanner sc = new Scanner(System.in);                         // 입력 받기
+        String n1 = sc.next();                                       // 첫 번째 직원 이름
+        int s1 = sc.nextInt();                                       // 첫 번째 직원 연봉
+        String n2 = sc.next();                                       // 두 번째 직원 이름
+        int s2 = sc.nextInt();                                       // 두 번째 직원 연봉
+        int raise = sc.nextInt();                                    // 인상 연봉 (추가 금액)
+        Employee e1 = new Employee(n1, s1);                          // Employee 객체 생성하여 e1이 참조
+        Employee e2 = new Employee(n2, s2);                          // Employee 객체 생성하여 e2가 참조
+        e1.setSalary(e1.getSalary() + raise);                        // e1이 참조하는 get 메서드에서 연봉 반환하여, 인상 연봉 합산하여 set 메서드로 다시 저장
+        System.out.println(e1.getSalary() + " " + e2.getSalary());   // 첫 번째 직원 연봉 , 두 번째 직원 연봉 출력
     }
 }
