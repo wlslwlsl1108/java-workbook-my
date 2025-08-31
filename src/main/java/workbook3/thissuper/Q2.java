e603b2077506eb1674c2a676f3b97198c262c8bc
jinhee kim <lsa68803@naver.com>
2025-08-31 23:58:43 +0900

    feat: [thissuper] Q2 구현

--- src/main/java/workbook3/thissuper/Q2.java
+++ src/main/java/workbook3/thissuper/Q2.java
@@ -6,21 +6,27 @@ class Box {
     int width, height, depth;
     Box() {
         // TODO: 매개변수 생성자 호출하여 1,1,1로 초기화
+        this.width = 1;                    // width 생성자 호출하여 1로 초기화
+        this.height = 1;                   // height 생성자 호출하여 1로 초기화
+        this.depth = 1;                    // depth 생성자 호출하여 1로 초기화
     }
     Box(int w, int h, int d) {
         // TODO: 멤버 초기화
+        this.width = w;                    // 입력받은 w 값으로 width 초기화
+        this.height = h;                   // 입력받은 h 값으로 height 초기화
+        this.depth = d;                    // 입력받은 d 값으로 depth 초기화
     }
 }
 
 public class Q2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int w = sc.nextInt();
-        int h = sc.nextInt();
-        int d = sc.nextInt();
-        Box b1 = new Box();
-        Box b2 = new Box(w, h, d);
-        System.out.println(b1.width + " " + b1.height + " " + b1.depth);
-        System.out.println(b2.width + " " + b2.height + " " + b2.depth);
+        Scanner sc = new Scanner(System.in);                               // 입력받기
+        int w = sc.nextInt();                                              // w 입력
+        int h = sc.nextInt();                                              // h 입력
+        int d = sc.nextInt();                                              // d 입력
+        Box b1 = new Box();                                                // Box() 객체 생성하여 b1이 참조
+        Box b2 = new Box(w, h, d);                                         // Box(w, h, d) 객체 생성하여 b2가 참조
+        System.out.println(b1.width + " " + b1.height + " " + b1.depth);   // b1가 참조하는 w / h / d 출력
+        System.out.println(b2.width + " " + b2.height + " " + b2.depth);   // b2가 참조하는 w / h / d 출력
     }
 }
