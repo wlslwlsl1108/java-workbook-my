d98bbd03e17efd16065178c1ad13941196f386b1
jinhee kim <lsa68803@naver.com>
2025-09-01 10:17:41 +0900

    feat: [accesslevel] Q6 구현

--- src/main/java/workbook3/accesslevel/Q6.java
+++ src/main/java/workbook3/accesslevel/Q6.java
@@ -2,24 +2,30 @@ package main.java.workbook3.accesslevel;
 
 import java.util.Scanner;
 
-class SecureFile {
-    private String fileName;
-    SecureFile(String fileName) {
-        this.fileName = fileName;
+class SecureFile {                                               // 클래스 정의
+    private String fileName;                                     // 인스턴스 필드 선언 -> private (외부 직접 접근 불가)
+
+    SecureFile(String fileName) {                                // 생성자 정의
+        this.fileName = fileName;                                // 전달받은 값으로 초기화
     }
-    public void read(boolean hasPermission) {
+    public void read(boolean hasPermission) {                    // read 메서드 정의 (권한 검증)
         // TODO: 권한이 있으면 파일 이름 출력, 없으면 "권한 없음" 출력
+        if(hasPermission) {                                      // 권한 있으면 (true)
+            System.out.println("Reading " + this.fileName);      // 파일 이름 출력
+        } else {                                                 // 권한 없으면 (false)
+            System.out.println("권한 없음");                       // "권한 없음" 출력
+        }
     }
 }
 
 public class Q6 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String file = sc.next();
-        String p1 = sc.next();
-        String p2 = sc.next();
-        SecureFile sf = new SecureFile(file);
-        sf.read(p1.equals("granted"));
-        sf.read(p2.equals("granted"));
+        Scanner sc = new Scanner(System.in);                      // 입력 받기
+        String file = sc.next();                                  // 파일 이름 입력
+        String p1 = sc.next();                                    // 첫 번째 권한 여부 입력
+        String p2 = sc.next();                                    // 두 번째 권한 여부 입력
+        SecureFile sf = new SecureFile(file);                     // 객체 생성하려 sf가 참조
+        sf.read(p1.equals("granted"));                            // p1 권한 검증 후 출력
+        sf.read(p2.equals("granted"));                            // p2 권한 검증 후 출력
     }
 }
